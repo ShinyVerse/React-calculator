b213dcac38308ff022450d1059b101b84847e3ad
Laura Jackson <playeronereadystart@gmail.com>
2018-12-13 15:25:56 +0000

    Add plus button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+      super(props);
+
+      this.state = {
+        count: 0
+      }
+    }
+
+    onPlus = () => {
+      let newCount = this.state.count + 1;
+      this.setState({count: newCount});
+    }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div> {this.state.count}
+          </div>
+          <button onClick={this.onPlus} >+</button>
         </header>
       </div>
     );
